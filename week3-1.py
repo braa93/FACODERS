cf909458d10f20868f058c9d008748c961a5a4bd
braa93 <baraa.odeh93@gmail.com>
2020-05-10 04:29:42 +0300

    Third challenge

---
+++ week3-1.py
@@ -0,0 +1,9 @@
+n=3
+print("Numbers from 1 to 10")
+guess=int(input("Guess the number"))
+while n!= guess :
+    print("guess wrong")
+    break
+if n== guess :
+    print("Great! you did it")
+    
